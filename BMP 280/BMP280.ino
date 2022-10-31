b48c0d1a0563eb684739c4619418b650865b18fa
vikstra <115671789+vikstra@users.noreply.github.com>
2022-10-31 19:17:25 -0400

    update

--- BMP 280/BMP280.ino
+++ BMP 280/BMP280.ino
@@ -1,10 +1,24 @@
-#include <../../CanSat_2023/BMP 280/BMP280.h>
+#include <BMP280.h> 
+
+BMP280 temperature_meter();
+BMP280 pressure_meter(); 
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600); 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //call
+
+  pressure_meter.setPres(); //analog read? 
+  Serial.print("Pressure: ");
+  Serial.println(pressure_meter.getPres());
+
+  temperature_meter.setPres(); //analog read?
+  Serial.print("Temperature: ");
+  Serial.println(temperature_meter.getTemp()); 
 
 }
