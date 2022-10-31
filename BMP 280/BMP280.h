b0494c1ad7f0f434657395aa1dbec345ebad604a
vikstra <115671789+vikstra@users.noreply.github.com>
2022-10-31 19:16:44 -0400

    update

--- BMP 280/BMP280.h
+++ BMP 280/BMP280.h
@@ -1,7 +1,14 @@
 #pragma once 
-#include <Adafruit_BMP280.h>
-class BMP280{ // do some research and see what it is called :: public thing for inhereitance 
-  // private
+#include "Adafruit_BMP280.h"
+class BMP280:: public Adafruit_BMP280{
+  Adafruit_BMP280 bmp; 
+  Adafruit_Sensor *bmp_temp;
+  Adafruit_Sensor *bmp_pressure;
+  // private 
   public:
     BMP280(); 
+    void getPres();
+    double setPres();
+    void getTemp();
+    double setTemp();
 }; 
